11654a4f283430210f8663af40ba1d14697589f6
Artur-rah <ferreira.artur@escola.pr.gov.br>
2025-08-08 10:07:06 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
-
 const palpites = document.querySelector('.palpites');
 const ultimoResultado = document.querySelector('.ultimoResultado');
-const baixoOuAlta
+const baixoOuAlto = document.querySelector('.baixoOuAlto');
+const envioPalpite = document.querySelector('.envioPalpite');
+const campoPalpite = document.querySelector('.campoPalpite');
+
+
+
+function verificarPalpite() {
+  const palpiteUsuario = Number(campoPalpite.value);
+  if (contagemPalpites === 1) {
+    palpites.textContent = "Palpites anteriores: ";
+  }
+
+palpites.textContent += palpiteUsuario + " ";
+
+if (palpiteUsuario === numeroAleatorio) {
+  ultimoResultado.textContent = "Parabéns! Você Acertou!";
+ultimoResultado.style.backgroundColor = "green";
+  baixoOuAlto.textContent = "";
+  finalizarJogo();
+}
+
